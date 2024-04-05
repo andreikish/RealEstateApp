507beb0b4f6cc3b92a3a2c6bf73177f82ea0f64a
andreikish <162219710+andreikish@users.noreply.github.com>
2024-04-05 12:20:41 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:workshoparrkrealestate/features/home/ui/desktop/home_desktop_page.dart';
+import 'package:workshoparrkrealestate/features/home/ui/mobile/home_mobile_page.dart';
+import 'package:workshoparrkrealestate/responsive/responsive_layout.dart';
+import 'responsive/responsive_layout.dart';
+import 'features/home/ui/desktop/home_desktop_page.dart';
+import 'features/home/ui/mobile/home_mobile_page.dart';
+
+void main() {
+  runApp(RealEstateApp());
+}
+
+class RealEstateApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Real Estate App',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: ResponsiveLayout(mobilePage: HomeMobilePage(), desktopPage: HomeDesktopPage()),
+    );
+  }
+}
+
